e88720a5346884140a931fd4722d59dfbbb54983
giselladcaballero-cpu <giselladcaballero@gmail.com>
2026-09-14 15:55:29 -0300

    fix: missing verticalScroll import in CrashReportScreen

--- app/src/main/java/com/vektorgo/app/MainActivity.kt
+++ app/src/main/java/com/vektorgo/app/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
